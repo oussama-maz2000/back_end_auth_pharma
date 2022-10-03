9075e44629a07a936e6678f2a8ae6355c602572d
Mazeghrane Oussama <loulououssama2015@gmail.com>
2022-10-03 22:50:57 +0100

    entre point of our app

---
+++ src/main/java/com/pharmacy/authenctication/pahrma/PahrmaAuthApplication.java
@@ -0,0 +1,13 @@
+package com.pharmacy.authenctication.pahrma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PahrmaAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PahrmaAuthApplication.class, args);
+	}
+
+}
